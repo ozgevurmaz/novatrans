eda9dda1ddfef7d5903e1a9e1923d451b3cb2d6f
Elif Özge <ozgevurmaz@gmail.com>
2025-08-11 15:13:11 +0300

    fixed build bug

--- app/services/[slug]/page.tsx
+++ app/services/[slug]/page.tsx
@@ -2,18 +2,15 @@ import { ServiceDetailSection } from "@/components/service/serviceDetailSection"
 import { servicesInfo } from "@/lib/constanst"
 
 type Props = {
-  params: {
-    slug: string
-  }
+  params: { slug: string }
 }
 
 export default async function ServiceDetails({ params }: Props) {
-
-  const service = await servicesInfo.find((s) => s.name === params.slug)
+  const service = servicesInfo.find((s) => s.name === params.slug);
 
   if (!service) {
-    return <div className="p-8 text-center">Service not found</div>
+    return <div className="p-8 text-center">Service not found</div>;
   }
 
-  return <ServiceDetailSection service={service} />
+  return <ServiceDetailSection service={service} />;
 }
